1cbe5472a4954f9479ebfb6420c67e67eea42b3c
KrebsGeorge <77137087+KrebsGeorge@users.noreply.github.com>
2024-11-27 16:45:28 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -4,7 +4,7 @@
 CFSPEED_EXEC="./CloudflareST"
 OS_TYPE=$(uname)
 ARCH_TYPE=$(uname -m)
-CLOUDFLARE_IP_URL="http://dnspod.tk/ip/"
+CLOUDFLARE_IP_URL="https://www.baipiao.eu.org/cloudflare/ips-v4"
 CLOUDFLARE_IP_FILE="Cloudflare.txt"
 CONFIG_FILE="config.conf"
 RESULT_FILE="result.csv"
